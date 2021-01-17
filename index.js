5ddd4faab76cd185621c8c8d75add631b82c7d25
itsvermaji <deepakverma.pb34@gmail.com>
2021-01-17 21:39:57 +0530

    chai and mocha tests added

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 
-const port = 3000
+const port = 8080
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   return res.send('This is changed immediately!')
 })
 
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`Server running at port ${port}`)
 })
+
+module.exports = app
