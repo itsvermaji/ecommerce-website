074df6638767c6fbaafdd09812e34c02de8b374d
itsvermaji <deepakverma.pb34@gmail.com>
2021-01-17 21:07:05 +0530

    Day 3 - Added eslint to enforce code style

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const express = require('express') ;
-const app = express() ;
+const express = require('express')
+const app = express()
 
+const port = 3000
 
-const port = 3000 ;
-
-app.get('/', function(req, res) {
-    return res.send('This is changed immediately!') ;
+app.get('/', function (req, res) {
+  return res.send('This is changed immediately!')
 })
 
-app.listen(port, function() {
-    console.log(`Server running at port ${port}`);
+app.listen(port, function () {
+  console.log(`Server running at port ${port}`)
 })
